e509a53a885e76cb12a1a9e0714010a663539993
Craig McAlister <craigmack704@gmail.com>
2019-06-28 00:51:27 -0400

    added and edited game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,35 @@
 // -----------------
 
 var pcLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+for (var i = 0; i < pcLetter.length; i++)
 
 // Global Variables
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessesLeftText = document.getElementById("guesses-left");
-var guesseSoFarText = document.getElementById("guesses-so-far");
+var guessesSoFarText = document.getElementById("guesses-so-far");
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var
+var userLetterText = [];
+var pcLetterText = [];
 
+document.onkeyup = function(event) {
 
-        var userLetter = event.key;
-        var pcLetter = pcChoice[Math.floor(Math.random() * pcChoice.length)];
-        if (userLetter === pcLetter) {
+        var userLetter = event.key.toLowerCase();
+        var pcChoice = pcLetter[Math.floor(Math.random() * pcLetter.length)];
+        
+        if (userLetter === pcChoice) {
         winsText++;
         winsText.textContent = wins;
+        return;
         } else {
-        loses++;
+        losses++;
         lossesText.textContent = losses;
-        }
-    
+        guessesLeft--;
+        guessesLeftText.textContent = guessesLeft;
+        } 
+            
+        userLetterText.textContent = userGuess
+        pcLetterText.textContent = pcChoice
+}
