0bb68e737a31fb1db002f0bd0e918ca536ed517c
Cmack704 <craigmack704@gmail.com>
2019-06-27 23:12:06 -0400

    added ids to index.html created variables for ids and finished style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,24 @@
 var pcLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
-document.onkeyup = function(event) {
-    var userLetter = event.key;
-    var pcLetter = pcChoice[Math.floor(Math.random() * pcChoice.length)];
+// Global Variables
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
+var guessesLeftText = document.getElementById("guesses-left");
+var guesseSoFarText = document.getElementById("guesses-so-far");
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var
 
-    if (userLetter === pcLetter) {
-        wins++;
-    } else {
+
+        var userLetter = event.key;
+        var pcLetter = pcChoice[Math.floor(Math.random() * pcChoice.length)];
+        if (userLetter === pcLetter) {
+        winsText++;
+        winsText.textContent = wins;
+        } else {
         loses++;
-    }
-}
+        lossesText.textContent = losses;
+        }
+    
